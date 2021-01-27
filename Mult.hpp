e86fe02a7534fc4ab48f2fddf2e2b635aa07e24b
Vinay Srinivasan <vsrin008@hammer.cs.ucr.edu>
2021-01-27 08:31:22 -0800

    Fixes to Mult constructor.

--- Mult.hpp
+++ Mult.hpp
@@ -7,7 +7,6 @@
 
 class Mult : public Operator {
 	public:
-		Mult(Base* left, Base* right) : left(left), right(right) {}
                 Mult(Base* left, Base* right) {this->left = left; this->right = right; } 
 		double evaluate() { return left->evaluate() * right->evaluate(); }
 		std::string stringify() { return left->stringify() + " * " + right->stringify(); }
